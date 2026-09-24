896054a33f645f6798954789ea249cf96c6dee5d
Joey Escareno <joe_escareno@hotmail.com>
2026-09-24 00:25:35 -0500

    updated pointer_structures.c

--- pointer_structures.c
+++ pointer_structures.c
@@ -15,13 +15,17 @@ typedef struct
 // control, status, and data set to 0
 void initialize_device( Device *device)
 {
-
+    // because device is a pointer to a Device, -> is correct operator.
+    device->control = 0;
+    device->status = 0;
+    device->data = 0;
 }
 
 
 // stores value into the device's data register
 void write_data(Device *device, uint32_t value)
 {
+    device->data = value;
 
 }
 
@@ -29,6 +33,7 @@ void write_data(Device *device, uint32_t value)
 // returns the current data register
 uint32_t read_data(Device *device)
 {
+    return device->data;
 
 }
 
@@ -36,16 +41,14 @@ uint32_t read_data(Device *device)
 int main()
 {
     // create device
-
+    Device device;
 
     // initialize it
-
+    initialize_device(&device);
 
     // write a value to data
+    write_data(&device, 12345);
 
-
-    // read the value back
-
-
-    // print it
+    // read value back and print it.
+    printf("The data register value is %d.\n", read_data(&device));
 }
